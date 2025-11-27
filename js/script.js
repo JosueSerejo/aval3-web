bb020f0072a99d8ffbadc6d9cc761322db5c873b
Josué Serejo <josueserejo5@gmail.com>
2025-11-27 09:16:03 -0300

    Feat: Nav e menu sanduíche

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//menu toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menuToggle');
+    const mainNav = document.getElementById('mainNav');
+    menuToggle.addEventListener('click', () => {
+        mainNav.classList.toggle('menu-open');
+        menuToggle.classList.toggle('open');
+    });
+});
