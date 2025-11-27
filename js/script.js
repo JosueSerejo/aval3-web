8c9b93f5888cc3f55cc7409d3edef3a8b566d059
Josué Serejo <josueserejo5@gmail.com>
2025-11-27 10:41:05 -0300

    Feat: Filtros e Barra de pesquisa

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-//menu toggle
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const mainNav = document.getElementById('mainNav');
-    menuToggle.addEventListener('click', () => {
-        mainNav.classList.toggle('menu-open');
-        menuToggle.classList.toggle('open');
+
+// MENU TOGGLE
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menuToggle");
+    const mainNav = document.getElementById("mainNav");
+
+    menuToggle.addEventListener("click", () => {
+        mainNav.classList.toggle("menu-open");
+        menuToggle.classList.toggle("open");
     });
 });
+
